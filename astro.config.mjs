98ec025d9a6ee8cda78262ca4dada995cd600c4c
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-02-25 23:15:43 -0500

    assorted improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ographiesresearch.github.io"
+    integrations: [icon()],
+    site: "https://ographiesresearch.github.io",
 });
